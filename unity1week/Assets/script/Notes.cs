8628482f7c06b04bfdc11d25d0efb5c1368ee48c
yutayuta4839-del <yutayuta4839@gmail.com>
2026-03-16 22:25:42 +0900

    notes/setfont

--- unity1week/Assets/script/Notes.cs
+++ unity1week/Assets/script/Notes.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 public class Notes : MonoBehaviour
 {
     [SerializeField] float speed = 2f;
+    [SerializeField] float targettime;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
         transform.position -= transform.up * speed * Time.deltaTime;
+        if(transform.position.y < -5)
+        {
+            Destroy(this.gameObject);
+        }
     }
+
+   
 }
---
+++ unity1week/Assets/script/NotesManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class MusicsyncManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
